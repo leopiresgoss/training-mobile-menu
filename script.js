373825942988fc085cb393c77416fe7072befd4a
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-06-23 11:44:04 -0600

    Refactor open and close menu logic

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
+function toggleHiddenElements() {
+  const elements = [
+    '.menu-bars',
+    'main',
+    'footer',
+    'h1',
+  ];
+
+  elements.forEach((element) => {
+    document.querySelector(element).classList.toggle('hide');
+  });
+}
+
+function toggleShownElements() {
+  const elements = [
+    '#desktop-nav',
+    '#desktop-nav ul',
+  ];
+
+  elements.forEach((element) => {
+    document.querySelector(element).classList.toggle('show');
+  });
+}
+
+function removeCloseBtn() {
+  const closeBtn = document.querySelector('.close-btn');
+  closeBtn.parentNode.removeChild(closeBtn);
+}
+
 function closeMenu() {
-  document.querySelector('.menu-bars').classList.remove('hide');
-  document.querySelector('#desktop-nav').classList.remove('show');
-  document.querySelector('#desktop-nav ul').classList.remove('show');
-  document.querySelector('main').classList.remove('hide');
-  document.querySelector('footer').classList.remove('hide');
-  document.querySelector('h1').classList.remove('hide');
   document.querySelector('header').classList.remove('menu-visible');
-  document.querySelector('.close-btn').classList.add('hide');
+  toggleShownElements();
+  toggleHiddenElements();
+  removeCloseBtn();
+}
+
+function addMenuListeners() {
+  document.querySelectorAll('.nav-link').forEach((link) => {
+    link.addEventListener('click', closeMenu);
+  });
+}
+
+function createCloseBtn() {
+  const closeBtn = document.createElement('button');
+  closeBtn.innerHTML = 'X';
+  closeBtn.classList.add('close-btn');
+  closeBtn.type = 'button';
+  document.querySelector('header').appendChild(closeBtn);
 }
 
 function openMenu() {
-  document.querySelector('.menu-bars').classList.add('hide');
-  document.querySelector('#desktop-nav').classList.add('show');
-  document.querySelector('#desktop-nav ul').classList.add('show');
-  document.querySelector('main').classList.add('hide');
-  document.querySelector('footer').classList.add('hide');
-  document.querySelector('h1').classList.add('hide');
   document.querySelector('header').classList.add('menu-visible');
-  document.querySelector('.close-btn').classList.remove('hide');
+  toggleHiddenElements();
+  toggleShownElements();
+  createCloseBtn();
+  addMenuListeners();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.menu-bars').addEventListener('click', openMenu);
-  document.querySelector('.close-btn').addEventListener('click', closeMenu);
 });
